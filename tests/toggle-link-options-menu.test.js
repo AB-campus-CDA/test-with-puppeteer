22f25be9813e3a9095fdc6459c848b86df3fc2d6
AurelBouchard <au.bouchard@gmail.com>
2024-02-15 15:12:34 +0100

    show hide test

--- tests/toggle-link-options-menu.test.js
+++ tests/toggle-link-options-menu.test.js
@@ -1,20 +1,60 @@
-const timeout = 1500;
+const timeout = 5000;
 
 // test d'un raccourcisseur d'URL
-describe("Shorten Anonymous", () => {
+describe("Show hide options", () => {
     let page;
 
-    // vérification du chargement de la page d'accueil
-    test('basic shorten', async () => {
+    test('show options', async () => {
         await page.goto('http://polr.stationmyr.net');
-        await page.waitForSelector('.long-link-input');
+
+        // options should be hidden
+        await page.waitForSelector('#options',
+            { visible: false, timeout: 200 });
+
+        //expect(displayed).toMatch(/\s*none/)
+
         await page.screenshot({path: './tests/img/hidden_menu.png'});
-        await page.waitForSelector('#show-link-option');
-        await page.$eval( '#shorten', el => el.click() );
-        await page.waitForSelector('input.result-box');
-        const val = await page.$eval('input.result-box', el => el.value);
-        expect(val).toMatch(/^https:\/\/polr\.stationmyr\.net\/[a-z0-9]+/);
-        await page.screenshot({path: './tests/img/shorten2.png'});
+        await page.waitForSelector('#show-link-options');
+
+        await page.$eval( '#show-link-options', el => el.click() );
+
+
+
+        let style = await page.$eval('#options', el => el.style.display)
+        expect(style).toMatch(/block/)
+
+        await page.screenshot({path: './tests/img/hidden_menu_open.png'});
+    }, timeout);
+
+
+
+    test('hide options', async () => {
+        await page.goto('http://polr.stationmyr.net');
+
+        // options should be hidden
+        await page.waitForSelector('#options',
+            { visible: false, timeout: 200 });
+
+        await page.screenshot({path: './tests/img/open_menu.png'});
+        await page.waitForSelector('#show-link-options');
+
+        await page.$eval( '#show-link-options', el => el.click() );
+
+        //animation delay then click again
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        await page.screenshot({path: './tests/img/open_menu_open.png'})
+
+        await page.$eval( '#show-link-options', el => el.click() );
+
+        //animation delay then check
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        await page.screenshot({path: './tests/img/open_menu_hidden.png'});
+
+        let style = await page.$eval('#options', el => el.style.display)
+        expect(style).toMatch("none")
+
     }, timeout);
 
 
